ede19ab99191fcaa2f3c5ce8dbd418bad00f7843
UrmilaVadi <gopi.ghadiya@gmail.com>
2021-08-03 10:47:43 +0300

    git flow work action

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -21,3 +21,17 @@ import './commands'
 Cypress.on('uncaught:exception', (err, runnable) => {
     return false;
   });
+
+import addContext from 'mochawesome/addContext'
+
+
+Cypress.on("test:after:run", (test, runnable) => {
+      
+      let videoName = Cypress.spec.name
+      videoName = videoName.replace('/.js.*', '.js')
+      const videoUrl = 'videos/' + videoName + '.mp4'
+  
+  
+      addContext({ test }, videoUrl)
+  });
+  
